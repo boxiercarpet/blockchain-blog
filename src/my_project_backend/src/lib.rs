491c289c15f33232e11176af4833594b4e86c9b1
Jakub Baran <boxiercarpet@gmail.com>
2024-12-03 16:32:55 +0000

    Add blog structure and update management functions to handle blog details

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,17 +1,20 @@
+use crate::blog::Blog;
 use std::cell::RefCell;
+mod blog;
 
 thread_local! {
-    static BLOGS: RefCell<Vec<String>> = RefCell::new(Vec::new());
+    static BLOGS: RefCell<Vec<Blog>> = RefCell::new(Vec::new());
 }
 
 #[ic_cdk::update]
-fn add_blog(new_blog: String) -> () {
+fn add_blog(title: String, date: u32, content: String, tags: Vec<String>) -> () {
+    let blog = Blog::new(title, date, content, tags);
     BLOGS.with(|blogs| {
-        blogs.borrow_mut().push(new_blog);
+        blogs.borrow_mut().push(blog);
     });
 }
 
 #[ic_cdk::query]
-fn get_blogs() -> Vec<String> {
+fn get_blogs() -> Vec<Blog> {
     BLOGS.with(|blogs| blogs.borrow().clone())
 }
